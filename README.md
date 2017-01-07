ed217f95e2dc1bbbc199a36fc81db2d58afdd41a
Maxime Rafaillac <maximerafaillac@gmail.com>
2017-01-07 19:01:48 +0100

    Auto style compression

--- README.md
+++ README.md
@@ -1,27 +1,14 @@
-# [Start Bootstrap](http://startbootstrap.com/) - [Creative](http://startbootstrap.com/template-overviews/creative/)
+# Maxime Rafaillac personal website
 
-[Creative](http://startbootstrap.com/template-overviews/creative/) is a one page creative theme for [Bootstrap](http://getbootstrap.com/) created by [Start Bootstrap](http://startbootstrap.com/).
-
-## Getting Started
-
-To begin using this template, choose one of the following options to get started:
-* [Download the latest release on Start Bootstrap](http://startbootstrap.com/template-overviews/creative/)
-* Clone the repo: `git clone https://github.com/BlackrockDigital/startbootstrap-creative.git`
-* Fork the repo
-
-## Bugs and Issues
-
-Have a bug or an issue with this template? [Open a new issue](https://github.com/BlackrockDigital/startbootstrap-creative/issues) here on GitHub or leave a comment on the [template overview page at Start Bootstrap](http://startbootstrap.com/template-overviews/creative/).
-
-## Creator
-
-Start Bootstrap was created by and is maintained by **[David Miller](http://davidmiller.io/)**, Owner of [Blackrock Digital](http://blackrockdigital.io/).
-
-* https://twitter.com/davidmillerskt
-* https://github.com/davidtmiller
-
-Start Bootstrap is based on the [Bootstrap](http://getbootstrap.com/) framework created by [Mark Otto](https://twitter.com/mdo) and [Jacob Thorton](https://twitter.com/fat).
-
-## Copyright and License
-
-Copyright 2013-2016 Blackrock Digital LLC. Code released under the [MIT](https://github.com/BlackrockDigital/startbootstrap-creative/blob/gh-pages/LICENSE) license.
+gulp-csso to minify CSS
+gulp-less to compile LESS into CSS
+gulp-csscomb to reorder and sort css attributes
+gulp-cssbeautify to clean spaces, carriage returns etc from CSS
+gulp-load-pluginsto
+gulp-rename to 
+gulp-autoprefixer to 
+gulp-uglify to minify JS
+gulp-concat : to concatenate Js files
+gulp-imagemin to minify images
+gulp-uncss to clean css
+gulp-header: to add header to files
--- gulpfile.js
+++ gulpfile.js
@@ -1,98 +1,69 @@
 var gulp = require('gulp');
-var less = require('gulp-less');
-var browserSync = require('browser-sync').create();
-var header = require('gulp-header');
-var cleanCSS = require('gulp-clean-css');
-var rename = require("gulp-rename");
-var uglify = require('gulp-uglify');
-var pkg = require('./package.json');
+var packageJson = require('./package.json');
 
-// Set the banner content
-var banner = ['/*!\n',
-    ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
-    ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
-    ' * Licensed under <%= pkg.license.type %> (<%= pkg.license.url %>)\n',
+// Include plugins
+var plugins = require('gulp-load-plugins')();
+//var browserSync = require('browser-sync');
+
+var header = ['/*!\n',
+    ' * <%= packageJson.title %> v<%= packageJson.version %> - <%= packageJson.homepage %>\n',
+    ' * Copyright 2016-' + (new Date()).getFullYear(), ' <%= packageJson.author %>\n',
     ' */\n',
     ''
 ].join('');
 
-// Compile LESS files from /less into /css
-gulp.task('less', function() {
-    return gulp.src('less/creative.less')
-        .pipe(less())
-        .pipe(header(banner, { pkg: pkg }))
-        .pipe(gulp.dest('css'))
-        .pipe(browserSync.reload({
-            stream: true
-        }))
+var source = './';
+var destination = './dist';
+
+//compile LESS, clean and beautify CSS
+gulp.task('css', function () {
+  //return gulp.src(source + '/assets/css/styles.less')
+  return gulp.src(source + 'less/*.less')
+    .pipe(plugins.less())
+    .pipe(plugins.csscomb())
+    .pipe(plugins.cssbeautify({indent: '  '}))
+    .pipe(plugins.header(header, { packageJson: packageJson }))
+    .pipe(plugins.csso())
+    .pipe(plugins.rename({
+        suffix: '.min'
+    }))
+    .pipe(gulp.dest(destination + '/css/'));
 });
 
-// Minify compiled CSS
-gulp.task('minify-css', ['less'], function() {
-    return gulp.src('css/creative.css')
-        .pipe(cleanCSS({ compatibility: 'ie8' }))
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest('css'))
-        .pipe(browserSync.reload({
-            stream: true
-        }))
+
+//Minify CSS
+gulp.task('minify', function () {
+  return gulp.src(destination + '/css/*.css')
+    .pipe(plugins.csso())
+    .pipe(plugins.rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest(destination + '/css/'));
 });
 
-// Minify JS
-gulp.task('minify-js', function() {
-    return gulp.src('js/creative.js')
-        .pipe(uglify())
-        .pipe(header(banner, { pkg: pkg }))
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest('js'))
-        .pipe(browserSync.reload({
-            stream: true
-        }))
+
+// Uglify and concats Js files
+gulp.task('js', function() {
+  return gulp.src(source + 'js/*.js')
+    .pipe(plugins.uglify())
+    .pipe(plugins.concat('global.min.js'))
+    .pipe(plugins.header(header, { packageJson: packageJson }))
+    .pipe(gulp.dest(destination + '/assets/js/'));
 });
 
-// Copy vendor libraries from /node_modules into /vendor
-gulp.task('copy', function() {
-    gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
-        .pipe(gulp.dest('vendor/bootstrap'))
+// Optimize images
+//gulp.task('img', function () {
+//  return gulp.src(source + '/assets/img/*.{png,jpg,jpeg,gif,svg}')
+//    .pipe(plugins.imagemin())
+//    .pipe(gulp.dest(destination + '/assets/img'));
+//});
 
-    gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
-        .pipe(gulp.dest('vendor/jquery'))
 
-    gulp.src(['node_modules/magnific-popup/dist/*'])
-        .pipe(gulp.dest('vendor/magnific-popup'))
-
-    gulp.src(['node_modules/scrollreveal/dist/*.js'])
-        .pipe(gulp.dest('vendor/scrollreveal'))
-
-    gulp.src([
-            'node_modules/font-awesome/**',
-            '!node_modules/font-awesome/**/*.map',
-            '!node_modules/font-awesome/.npmignore',
-            '!node_modules/font-awesome/*.txt',
-            '!node_modules/font-awesome/*.md',
-            '!node_modules/font-awesome/*.json'
-        ])
-        .pipe(gulp.dest('vendor/font-awesome'))
-})
-
-// Run everything
-gulp.task('default', ['less', 'minify-css', 'minify-js', 'copy']);
-
-// Configure the browserSync task
-gulp.task('browserSync', function() {
-    browserSync.init({
-        server: {
-            baseDir: ''
-        },
-    })
-})
-
-// Dev task with browserSync
-gulp.task('dev', ['browserSync', 'less', 'minify-css', 'minify-js'], function() {
-    gulp.watch('less/*.less', ['less']);
-    gulp.watch('css/*.css', ['minify-css']);
-    gulp.watch('js/*.js', ['minify-js']);
-    // Reloads the browser whenever HTML or JS files change
-    gulp.watch('*.html', browserSync.reload);
-    gulp.watch('js/**/*.js', browserSync.reload);
+// Watching less files
+gulp.task('watch', function () {
+  gulp.watch(source + 'less/*.less', ['css']);
 });
+
+gulp.task('build', ['css', 'js']);
+gulp.task('prod', ['build',  'minify']);
+gulp.task('default', ['build']);
